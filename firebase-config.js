0d07f963d0a23680a2b7415dfd9ef41e06a8f4e4
NeinMon <hoangdhse184661@fpt.edu.vn>
2026-05-26 14:13:37 +0700

    Enable Firebase leaderboard config

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,14 @@
 // Dien thong tin Firebase Web App cua ban vao day.
 // Khi enabled = true va config hop le, leaderboard se dong bo public qua Firestore.
 window.TRUTH_FIREBASE_SETTINGS = {
-  enabled: false,
+  enabled: true,
   config: {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyBfWQ6UgyYBKuJIHO-13b0unwH2fTzSgMU",
+    authDomain: "truy-tim-chan-ly.firebaseapp.com",
+    projectId: "truy-tim-chan-ly",
+    storageBucket: "truy-tim-chan-ly.firebasestorage.app",
+    messagingSenderId: "480919348300",
+    appId: "1:480919348300:web:c66d5ef25b83463f9b00e9",
+    measurementId: "G-M7X1735V9C"
   }
 };
